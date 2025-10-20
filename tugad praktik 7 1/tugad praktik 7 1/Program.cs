5b2c8c635714319a218313c5fdfa2e348e07b24a
adyatma009 <adyatmaarfa552@gmail.com>
2025-10-20 14:57:52 +0700

    Add files via upload

---
+++ tugad praktik 7 1/tugad praktik 7 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace praktik_7._3_fungsi
+{
+    internal class Program
+    {
+        static int kali(int a, int b)
+        {
+            return a + b;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Masukkan angka pertama: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Masukkan angka kedua: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            int hasil = kali(x, y);
+            Console.WriteLine("Hasil perkalian: " + hasil);
+        }
+    }
+}
